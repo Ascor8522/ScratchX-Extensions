b23f0d21c5071290566ada5b4c6c7cb2ee41a8c7
smrman <vinoa.89.jeux@gmail.com>
2018-08-21 19:46:09 +0200

    added find word function

--- StringTools/StringTools.js
+++ StringTools/StringTools.js
@@ -8,18 +8,42 @@
         return {status: 2, msg: 'Ready'};
     };
 
+    /**
+     * Returns weather a sentence contains a word
+     * @param {string} sentence a sentence
+     * @param {string} word a word
+     * Trailing spaces are removed from the word
+     */
     ext.contains = function(sentence, word) {
-        return sentence.toString().split(' ').includes(word.toString());
+        return sentence.toString().split(' ').includes(word.toString().replace(/^\s+|\s+$/gm, ''));
+    }
+
+    /**
+     * Returns the index of a word in the sentence
+     * Returns -1 if the word doesn't appear in the sentence
+     * @param {string} sentence 
+     * @param {string} string 
+     * Trailing spaces are removed from the word
+     * Index begins at 1 and not at 0 (easier for the kids)
+     */
+    ext.find = function(word, sentence) {
+        var array = sentence.toString().split(' ');
+        if(array.includes(word.toString().replace(/^\s+|\s+$/gm, ''))) {
+            return array.indexOf(word.toString().replace(/^\s+|\s+$/gm, '')) + 1;
+        } else {
+            return -1;
+        }
     }
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['b','sentence %s contains word %s','contains', 'I love Scratch', 'love']
+            ['b','sentence %s contains word %s','contains', 'I love Scratch', 'love'],
+            ['r', 'index of word %s in the sentence %s', 'a', 'a b c d e f']
         ],
         displayName: 'Advanced String Operators'
     };
     
     // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext);
+    ScratchExtensions.register('Advanced String Operators', descriptor, ext);
 })({});
