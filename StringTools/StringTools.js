ca8edc89e4783fd7171b5d2b9fb5ab0742dc7135
smrman <vinoa.89.jeux@gmail.com>
2018-08-21 21:53:01 +0200

    added findChar + format function

--- StringTools/StringTools.js
+++ StringTools/StringTools.js
@@ -10,8 +10,8 @@
 
     /**
      * Returns weather a sentence contains a word
-     * @param {string} sentence a sentence
-     * @param {string} word a word
+     * @param {string} sentence a sentence that might contain the word
+     * @param {string} word a word that might be contained in the sentence
      * Trailing spaces are removed from the word
      */
     ext.contains = function(sentence, word) {
@@ -21,17 +21,42 @@
     /**
      * Returns the index of a word in the sentence
      * Returns -1 if the word doesn't appear in the sentence
-     * @param {string} sentence 
-     * @param {string} string 
+     * @param {string} sentence a sentence that might contain the word
+     * @param {string} word a word we want to know the index of
      * Trailing spaces are removed from the word
      * Index begins at 1 and not at 0 (easier for the kids)
      */
     ext.find = function(word, sentence) {
-        var array = sentence.toString().split(' ');
-        if(array.includes(word.toString().replace(/^\s+|\s+$/gm, ''))) {
-            return array.indexOf(word.toString().replace(/^\s+|\s+$/gm, '')) + 1;
-        } else {
-            return -1;
+        return sentence.toString().split(' ').includes(word.toString().replace(/^\s+|\s+$/gm, '')) ? sentence.toString().split(' ').indexOf(word.toString().replace(/^\s+|\s+$/gm, '')) + 1 : -1;
+    }
+
+    /**
+     * Returns the index of a char in the string
+     * Returns -1 if the char doesn't appear in the string
+     * @param {char} char a char we want to know the index of
+     * @param {string} string a string that might contain the char
+     * Index begins at 1 and not at 0 (easier for the kids)
+     */
+    ext.findChar = function(char, string) {
+        return string.toString().indexOf(char) > -1 ? string.toString().indexOf(char) + 1 : -1;
+    }
+
+    /**
+     * Format the text depending on the format parameter
+     * Text can be all lowercase, all uppercase or with the first letter uppercase
+     * @param {string} text the text to format
+     * @param {string} format the selected format
+     */
+    ext.format = function(text, format) {
+        switch(format) {
+            case 'all lowercase':
+                return text.toString().toLowerCase();
+            case 'all uppercase':
+                return text.toString().toUpperCase();
+            case '1st letter uppercase':
+                return text.slice(0,1).toUpperCase() + text.slice(1);
+            default:
+                return text;
         }
     }
 
@@ -39,8 +64,13 @@
     var descriptor = {
         blocks: [
             ['b','sentence %s contains word %s','contains', 'I love Scratch', 'love'],
-            ['r', 'index of word %s in the sentence %s', 'a', 'a b c d e f']
+            ['r', 'index of word %s in the sentence %s', 'find' ,'a', 'a b c d e f'],
+            ['r', 'index of char %s in the string %s', 'find' ,'h', 'woohoo!'],
+            ['r', 'format %s %s.formatFomat', 'format', 'hello']
         ],
+        menus:{
+            formatFomat:['all lowercase', 'all uppercase', '1st letter uppercase']
+        },
         displayName: 'Advanced String Operators'
     };
     
