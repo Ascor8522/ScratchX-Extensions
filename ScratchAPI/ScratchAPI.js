7c44c1d3f515a3897c42a1d7b1bdf1daa266bf1f
smrman <vinoa.89.jeux@gmail.com>
2018-09-05 16:37:08 +0200

    added message count

--- ScratchAPI/ScratchAPI.js
+++ ScratchAPI/ScratchAPI.js
@@ -18,44 +18,83 @@
         }
     };
 
-    ext.getUser = function(username, info, callback) {
-        if (!navigator.onLine) return "No internet connection";
-        if(!username) return "No username provided";
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://api.scratch.mit.edu/users/" + username.toString(), true);
-        xhr.onload = function (e) {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    var answer = JSON.parse(xhr.responseText);
-                    switch (info) {
-                        case "id":
-                            callback(answer.id.toString());
-                            break;
-                        case "username":
-                            callback(answer.username.toString());
-                            break;
-                        case "join date":
-                            callback(new Date(answer.history.joined.toString().slice(0, -1)).toLocaleDateString());
-                            break;
-                        case "status":
-                            callback(answer.profile.status.toString());
-                            break;
-                        case "bio":
-                            callback(answer.profile.bio.toString());
-                            break;
-                        case "country":
-                            callback(answer.profile.country.toString());
-                            break;
+    /**
+     * Returns infos on the user
+     * @param {string} username The provided username
+     * @param {string} info The info to look for.
+     * @param {string} callback Callback for async function.
+     */
+    ext.getUser = function (username, info, callback) {
+        if (!navigator.onLine) callback("No internet connection");
+        if (!username) callback("No username provided");
+        if (info == "message count") {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", `https://api.scratch.mit.edu/proxy/users/${username.toString()}/activity/count`, true);
+            xhr.onload = function (e) {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        var answer = JSON.parse(xhr.responseText);
+                        if (answer.hasOwnProperty("code") && answer.code.toString() == "NotFound") callback("User not found");
+                        callback(answer.msg_count.toString());
+                    } else if(xhr.status == 429) {
+                        callback("Too many request. Max 10 req / sec");
+
+                    } else {
+                        return "Error :\n" + xhr.error;
                     }
-                } else {
-                    return "Error :\n" + xhr.error;
                 }
-            }
-        };
-        xhr.onerror = function (e) {
-        console.error(xhr.statusText);
-        };
-        xhr.send(null);
+            };
+            xhr.onerror = function (e) {
+                callback("Error:\n" + xhr.statusText);
+                console.error(xhr.statusText);
+            };
+            xhr.send(null);
+        } else {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "https://api.scratch.mit.edu/users/" + username.toString(), true);
+            xhr.onload = function (e) {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        var answer = JSON.parse(xhr.responseText);
+                        if (answer.hasOwnProperty("code") && answer.code.toString() == "NotFound") callback("User not found");
+                        switch (info) {
+                            case "id":
+                                callback(answer.id.toString());
+                                break;
+                            case "username":
+                                callback(answer.username.toString());
+                                break;
+                            case "join date":
+                                callback(new Date(answer.history.joined.toString().slice(0, -1)).toLocaleDateString());
+                                break;
+                            case "status":
+                                callback(answer.profile.status.toString());
+                                break;
+                            case "bio":
+                                callback(answer.profile.bio.toString());
+                                break;
+                            case "country":
+                                callback(answer.profile.country.toString());
+                                break;
+                            case "followers":
+                                break;
+                            case "following":
+                                break;
+                        }
+                    } else if(xhr.status == 429) {
+                        callback("Too many request. Max 10 req / sec");
+
+                    } else {
+                        return "Error :\n" + xhr.error;
+                    }
+                }
+            };
+            xhr.onerror = function (e) {
+                callback("Error:\n" + xhr.statusText);
+                console.error(xhr.statusText);
+            };
+            xhr.send(null);
+        }
     }
 
     // Block and block menu descriptions
@@ -64,7 +103,7 @@
             ["R", "get user %s %m.getUser", "getUser", "mres", "id"]
         ],
         menus: {
-            "getUser":["id", "username", "join date", "status", "bio", "country"]
+            "getUser": ["id", "username", "join date", "status", "bio", "country", "followers", "following", "message count"]
         },
         displayName: "ScratchAPI"
     };
